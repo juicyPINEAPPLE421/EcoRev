c9c0a6b862588368884705fc049526f58a89f268
juicyPINEAPPLE421 <143954942+juicyPINEAPPLE421@users.noreply.github.com>
2023-09-17 12:32:23 -0300

    Update app.py
    made the lcd screen refresh without wiggle

--- app.py
+++ app.py
@@ -19,12 +19,14 @@ class MainWindow(QMainWindow):
 
         layout = QHBoxLayout() #makes the layout of the app to be horizontal
 
+        self.index = 0
+
         self.displayer = QLCDNumber() #creates the LCD widget
         layout.addWidget(self.displayer) #adds LCD to the layout
 
         slider = QSlider() #creates the slider widget
         slider.setMinimum(0) #sets values to the slider
-        slider.setMaximum(25)
+        slider.setMaximum(23)
         slider.setSingleStep(1)
 
         self.slider_timer = None  # Initialize the timer
@@ -44,31 +46,32 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget) #centers all the widgets
 
     def value_changed(self, i):
-        if self.start_time is not None:
-            elapsed_time = time.time() - self.start_time #gets the time for
-            product = i * elapsed_time #finds speed by mutiplying throttle percentage and time
-            self.displayer.display(product)
-            if i > 6: #warns the users if they are accelerating too fast (5 seconds to accelerate your vehicle up to 20 kilometres per hour from a stop)
-                self.warning_label.setText("You are going too fast")
-            else:
-                self.warning_label.clear()           
+        self.index = i        
 
-    def slider_pressed_event(self):
+    def slider_pressed_event(self): 
         print("Slider pressed")
         self.start_time = time.time()
         self.slider_timer = QTimer()  # Create a QTimer for handling the time
-        self.slider_timer.timeout.connect(self.print_elapsed_time)
-        self.slider_timer.start(1000)  # Start the timer when the slider is pressed
+        self.slider_timer.timeout.connect(self.refresh_lcd)
+        self.slider_timer.start(1000)  # Start the timer when the slider is pressed  
 
-    def slider_released_event(self):
+    def slider_released_event(self): 
         print("Slider released")
+        self.displayer.display(0) #resets lcd screen when slider is released
+        self.warning_label.clear()
         if self.slider_timer is not None:
             self.slider_timer.stop()  # Stop the timer when the slider is released
 
-    def print_elapsed_time(self): #Displays elasped time in the terminal
+    def refresh_lcd(self): #refreshes the lcd screen
         if self.start_time is not None:
-            elapsed_time = time.time() - self.start_time
-            print(f"Elapsed Time: {elapsed_time:.2f} seconds")
+            elapsed_time = time.time() - self.start_time #gets the time that elapsed
+            product = self.index * elapsed_time #finds speed by mutiplying throttle percentage and time
+            self.displayer.display(product)
+            print (elapsed_time) #prints elpased time in the terminal
+            if self.index > 5: #warns the users if they are accelerating too fast (5 seconds to accelerate your vehicle up to 20 kilometres per hour from a stop)
+                self.warning_label.setText("You are going too fast")
+            else:
+                self.warning_label.clear() 
 
 app = QApplication(sys.argv) #creates the app
 window = MainWindow() #creates the window of the app
